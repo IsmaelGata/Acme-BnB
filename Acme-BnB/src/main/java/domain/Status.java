e394865d8c691a6c1ec422da31e4a5262fb0fa8f
rafgardom <rafgardom2@alum.us.es>
2017-02-15 17:53:10 +0100

    Cambio status

--- Acme-BnB/src/main/java/domain/Status.java
+++ Acme-BnB/src/main/java/domain/Status.java
@@ -2,5 +2,24 @@
 package domain;
 
 public enum Status {
-	PENDING, ACEPTED, DENIED
+
+	PENDING("PENDING", "PENDIENTE"), ACEPTED("ACEPTED", "ACEPTADA"), DENIED("DENIED", "DENEGADA");
+
+	private final String	name;
+	private final String	spanishName;
+
+
+	private Status(String name, String spanishName) {
+		this.name = name;
+		this.spanishName = spanishName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSpanishName() {
+		return spanishName;
+	}
+
 }
