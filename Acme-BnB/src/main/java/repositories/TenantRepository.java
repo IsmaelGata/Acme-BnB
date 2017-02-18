d72fa0957d54ea87b8822b421d16bf868dd2618b
Salvador <Student@10.0.2.15>
2017-02-18 14:14:45 +0100

    Correcion queries

--- Acme-BnB/src/main/java/repositories/TenantRepository.java
+++ Acme-BnB/src/main/java/repositories/TenantRepository.java
@@ -30,13 +30,13 @@ public interface TenantRepository extends JpaRepository<Tenant, Integer> {
 	
 	
 	//Dashboard
-	@Query("select b.tenant from Book b where b.status=1 group by b.tenant having max(b.tenant)>=(select max(b2.tenant) from Book b2 where b2.status=1")
+	@Query("select b.tenant from Book b where b.status=1 group by b.tenant having max(b.tenant)>=(select max(b2.tenant) from Book b2 where b2.status=1)")
 	Collection<Tenant> tenantMoreRequestAcepted();
 	
-	@Query("select b.tenant from Book b where b.status=2 group by b.tenant having max(b.tenant)>=(select max(b2.tenant) from Book b2 where b2.status=2")
+	@Query("select b.tenant from Book b where b.status=2 group by b.tenant having max(b.tenant)>=(select max(b2.tenant) from Book b2 where b2.status=2)")
 	Collection<Tenant> tenantMoreRequestDenied();
 	
-	@Query("select b.tenant from Book b where b.status=0 group by b.tenant having max(b.tenant)>=(select max(b2.tenant) from Book b2 where b2.status=0")
+	@Query("select b.tenant from Book b where b.status=0 group by b.tenant having max(b.tenant)>=(select max(b2.tenant) from Book b2 where b2.status=0)")
 	Collection<Tenant> tenantMoreRequestPending();
 	
 	@Query("select 1.0*(count(b)/(select count(b1) from Tenant t1 join t1.books b1)) from Tenant t join t.books b where b.status=1 group by t")
